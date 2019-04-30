d83f51a36847c48a868f913679a3bcde5251c98b
andrey.matveev <andrey.matveev@jetbrains.com>
2019-04-30 20:50:35 +0700

    Build triangles

--- main.cpp
+++ main.cpp
@@ -1,4 +1,4 @@
-#include <GL/glut.h>
+#include <GLUT/glut.h>
 #include <vector>
 #include <iostream>
 #include <fstream>
@@ -7,13 +7,35 @@
 #include <random>
 #include <functional>
 
+using std::vector;
+using std::pair;
+
+const int K = 5;
+
 struct Vertex
 {
     float x, y, z, w;
     float r, g, b, a;
 };
-std::vector< Vertex > verts;
-std::vector< Vertex > verts_bunny;
+vector< Vertex > verts;
+vector< Vertex > verts_bunny;
+vector<std::vector<int>> bunny_g;
+
+#define sqr(x) ((x) * (x))
+
+float get_dist(Vertex a, Vertex b) {
+    return sqrt(sqr(a.x - b.x) + sqr(a.y - b.y) + sqr(a.z - b.z));
+}
+
+struct Triangle {
+    Vertex a, b, c;
+
+    Triangle(Vertex a, Vertex b, Vertex c): a(a), b(b), c(c) {}
+};
+std::vector<Triangle> triangles_bunny;
+
+std::string os = "macOS";
+std::string who = "bunny";
 
 void fillVerts()
 {
@@ -84,13 +106,46 @@ void timer( int extra )
     glutTimerFunc( 16, timer, 0 );
 }
 
+void build_triangles_bunny() {
+    bunny_g.assign(verts_bunny.size(), std::vector<int>());
+    for (int i = 0; i < verts_bunny.size(); ++i) {
+        std::cerr << i << std::endl;
+        vector<pair<float, int>> ps;
+        for (int j = 0; j < verts_bunny.size(); ++j) {
+            if (i == j) {
+                continue;
+            }
+            ps.emplace_back(get_dist(verts_bunny[i], verts_bunny[j]), j);
+        }
+        std::sort(ps.begin(), ps.end());
+        for (int j = 0; j < std::min(K, static_cast<int>(ps.size())); ++j) {
+            bunny_g[i].push_back(ps[j].second);
+        }
+    }
+
+    for (int i = 0; i < bunny_g.size(); ++i) {
+        for (int j = 0; j < bunny_g[i].size(); ++j) {
+            int n1 = bunny_g[i][j];
+            for (int k = 0; k < bunny_g[n1].size(); ++k) {
+                int n2 = bunny_g[n1][k];
+                if (n2 == i || n2 == n1) {
+                    continue;
+                }
+                if (std::find(bunny_g[i].begin(), bunny_g[i].end(), n2) != bunny_g[i].end()) {
+                    triangles_bunny.emplace_back(verts_bunny[i], verts_bunny[n1], verts_bunny[n2]);
+                }
+            }
+        }
+    }
+}
+
 void readVerticesFromPly() {
     std::mt19937 gen;
     std::mt19937::result_type seed = time(0);
     auto real_rand = std::bind(std::uniform_real_distribution<float>(0,1),
                                std::mt19937(seed));
 
-    auto in = std::ifstream("/home/mano/CLionProjects/OpenGL/bunny/bun000.ply");
+    auto in = std::ifstream("../bunny/bun000.ply");
     std::string dummy;
     for (int i = 0; i < 24; ++i) {
         getline(in, dummy);
@@ -99,15 +154,20 @@ void readVerticesFromPly() {
     for (int i = 0; i < 40256; ++i) {
         in >> x >> y >> z;
         Vertex cur;
-        cur.x = x * 200;
-        cur.y = z * 200;
-        cur.z = y * 200;
+        cur.x = x;
+        cur.y = z;
+        cur.z = y;
         cur.a = 0.5f;
         cur.r = real_rand();
         cur.g = real_rand();
         cur.b = real_rand();
+//        cur.r = 0.5;
+//        cur.g = 0.9;
+//        cur.b = 0.2;
         verts_bunny.push_back(cur);
     }
+
+    build_triangles_bunny();
 }
 
 template<class T>
@@ -153,34 +213,45 @@ void display(void)
     glLoadIdentity();
     const double w = glutGet( GLUT_WINDOW_WIDTH );
     const double h = glutGet( GLUT_WINDOW_HEIGHT );
-    gluPerspective( 60.0, w / h, 1.0, 10000.0 );
+    gluPerspective( 60.0, w / h, 0.001, 1000.0 );
 //    gluPerspective( 60.0, w / h, 0.001, 1.0 );
 
     glMatrixMode(GL_MODELVIEW);
     glLoadIdentity();
 //    gluLookAt( 70, 70, 70, 0, 0, 0, 0, 0, 1 );
-    gluLookAt( 40, 40, 40, 0, 0, 20, 0, 0, 1 );
+    gluLookAt( 0.1, 0.3, 0.2, 0, 0, 0.1, 0, 0, 1 );
 
     glRotatef( angle, 0, 0, 1 );
 
     // draw curve
-    glEnableClientState( GL_VERTEX_ARRAY );
-    glEnableClientState( GL_COLOR_ARRAY );
-    glVertexPointer( 3, GL_FLOAT, sizeof( Vertex ), &verts_bunny[0].x );
-    glColorPointer( 4, GL_FLOAT, sizeof( Vertex ), &verts_bunny[0].r );
-    glDrawArrays( GL_LINE_STRIP, 0, verts_bunny.size() );
-    glDisableClientState( GL_VERTEX_ARRAY );
-    glDisableClientState( GL_COLOR_ARRAY );
-
-//    for (int i = 0; i < 40000; i += 3) {
-//        glBegin(GL_TRIANGLES);
-//        for (int j = 0; j < 3; ++j) {
-//            auto v = verts_bunny[i + j];
-//            glColor3f( v.r, v.g, v.b );
-//            glVertex3f( v.x, v.y, v.z );
-//        }
-//        glEnd();
+//    glEnableClientState( GL_VERTEX_ARRAY );
+//    glEnableClientState( GL_COLOR_ARRAY );
+//    if (who == std::string("bunny")) {
+//        glVertexPointer( 3, GL_FLOAT, sizeof( Vertex ), &verts_bunny[0].x );
+//        glColorPointer( 4, GL_FLOAT, sizeof( Vertex ), &verts_bunny[0].r );
+//        glDrawArrays( GL_LINE_STRIP, 0, verts_bunny.size() );
+//    } else {
+//        glVertexPointer( 3, GL_FLOAT, sizeof( Vertex ), &verts[0].x );
+//        glColorPointer( 4, GL_FLOAT, sizeof( Vertex ), &verts[0].r );
+//        glDrawArrays( GL_LINE_STRIP, 0, verts.size() );
 //    }
+//    glDisableClientState( GL_VERTEX_ARRAY );
+//    glDisableClientState( GL_COLOR_ARRAY );
+
+    for (auto tr : triangles_bunny) {
+        glBegin(GL_TRIANGLES);
+
+        glColor3f( tr.a.r, tr.a.g, tr.a.b );
+        glVertex3f( tr.a.x, tr.a.y, tr.a.z );
+
+        glColor3f( tr.b.r, tr.b.g, tr.b.b );
+        glVertex3f( tr.b.x, tr.b.y, tr.b.z );
+
+        glColor3f( tr.c.r, tr.c.g, tr.c.b );
+        glVertex3f( tr.c.x, tr.c.y, tr.c.z );
+
+        glEnd();
+    }
 
     // TODO: find nearest points and connect it with each other
 
